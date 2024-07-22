d1f0d1f481a0bb1e4197c9155df98155eea151c4
Ahmadon12 <dedoraahmad@gmail.com>
2024-07-22 15:37:52 +0100

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
   if (!text) throw ` مثال :\n*.song* ماهر زين`
   let res = await yts(text)
   let vid = res.videos[0]
-  await conn.sendMessage(m.chat, { react: { text: "🌏","🤍","☑️",key: m.key,}
+  await conn.sendMessage(m.chat, { react: { text: "🌏",key: m.key,}
   })  
   if (!vid) throw 'لم يتم العثور عليه، حاول عكس العنوان والمؤلف'
   let { title, description, thumbnail, videoId, durationH, viewH, publishedTime } = vid
